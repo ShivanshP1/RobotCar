58a94e8fd610dd78f3fe981ba3d7c3589f7ebd3f
Mattastrophic0 <mattk2227@gmail.com>
2025-03-11 18:01:28 -0400

    Update LEDtest.py
    fuckass

--- LEDtest.py
+++ LEDtest.py
@@ -4,8 +4,10 @@ import time
 GPIO.setmode(GPIO.BCM)
 # Setup GPIO pin 18 as an output
 GPIO.setup(18, GPIO.OUT)
+print("ass")
 try:
     while True:
+        print("fuck")
         GPIO.output(18, GPIO.HIGH) # Turn LED on
         time.sleep(1)              # Wait 1 second
         GPIO.output(18, GPIO.LOW)  # Turn LED off
