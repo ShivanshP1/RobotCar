038921b48fba21e91bf4289eff9f719f8572ce6b
Mattastrophic0 <mattk2227@gmail.com>
2025-03-11 16:55:12 -0400

    Update LEDtest.py
    switch to pin 4

--- LEDtest.py
+++ LEDtest.py
@@ -3,12 +3,12 @@ import time
 from robot_hat import Pin
 
 # Create Pin object with numeric pin numbering and default input pullup enabled
-d2 = Pin(2, Pin.OUT)
-d1 = Pin(1, Pin.OUT)
+d4 = Pin(2, Pin.OUT)
+#d1 = Pin(1, Pin.OUT)
 # Create Pin object with named pin numbering
-d1.low()
+#d1.low()
 for i in range(10):
-  d2.high()
+  d4.high()
   time.sleep(1)
-  d2.low()
+  d4.low()
   time.sleep(1)
