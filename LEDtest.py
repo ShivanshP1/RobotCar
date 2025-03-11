1c06170545e2b53015336fb86fae00be3d8a3a4f
Mattastrophic0 <mattk2227@gmail.com>
2025-03-11 17:57:25 -0400

    Update LEDtest.py
    fgbfgbrhvuhfdauso

--- LEDtest.py
+++ LEDtest.py
@@ -3,13 +3,12 @@ import time
 # Use BCM numbering for the GPIO pins
 GPIO.setmode(GPIO.BCM)
 # Setup GPIO pin 18 as an output
-GPIO.setup(18, GPIO.OUT)
-​
+GPIO.setup(17, GPIO.OUT)
 try:
     while True:
-        GPIO.output(18, GPIO.HIGH) # Turn LED on
+        GPIO.output(17, GPIO.HIGH) # Turn LED on
         time.sleep(1)              # Wait 1 second
-        GPIO.output(18, GPIO.LOW)  # Turn LED off
+        GPIO.output(17, GPIO.LOW)  # Turn LED off
         time.sleep(1)              # Wait 1 second
 except KeyboardInterrupt:
     pass
