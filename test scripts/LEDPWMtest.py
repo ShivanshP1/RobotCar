f8a96ae93fed02bd89d5e1daa893f1d7ddeb7899
Mattastrophic0 <mattk2227@gmail.com>
2025-03-17 17:26:17 -0400

    Update LEDPWMtest.py

--- test scripts/LEDPWMtest.py
+++ test scripts/LEDPWMtest.py
@@ -4,7 +4,7 @@ from robot_hat import PWM
 # Create PWM object with numeric pin numbering and default input pullup enabled
 p0 = PWM(5)
 # Create PWM object with named pin numbering
-p1 = PWM('P4')
+#p1 = PWM('P1')
 
 
 # Set frequency will automatically set prescaller and period
@@ -18,16 +18,3 @@ print(f"Prescaler: {p0.prescaler()}")
 print(f"Period: {p0.period()}")
 # Set pulse width to 50%
 p0.pulse_width_percent(100)
-
-# Or set prescaller and period, will get a frequency from:
-# frequency = PWM.CLOCK / prescaler / period
-# With this setup you can tune the period as you wish.
-# set prescaler to 64
-p1.prescaler(64)
-# set period to 4096 ticks
-p1.period(4096)
-print(f"Frequence: {p1.freq()} Hz")
-print(f"Prescaler: {p1.prescaler()}")
-print(f"Period: {p1.period()}")
-# Set pulse width to 2048 which is also 50%
-p1.pulse_width(2048)
