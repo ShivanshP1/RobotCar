926f0dbd2519fe7db518fadd6dbd0ae8077a6582
Mattastrophic0 <mattk2227@gmail.com>
2025-03-17 17:44:09 -0400

    Update LEDPWMtest.py

--- test scripts/LEDPWMtest.py
+++ test scripts/LEDPWMtest.py
@@ -2,20 +2,29 @@
 from robot_hat import PWM
 
 # Create PWM object with numeric pin numbering and default input pullup enabled
-p0 = PWM(5)
+p4 = PWM(4)
+p5 = PWM(5)
+
 # Create PWM object with named pin numbering
 #p1 = PWM('P1')
-
-#penis
 
 # Set frequency will automatically set prescaller and period
 # This is easy for device like Buzzer or LED, which you care
 # about the frequency and pulse width percentage.
 # this usually use with pulse_width_percent function.
 # Set frequency to 1000Hz
-p0.freq(1000)
-print(f"Frequence: {p0.freq()} Hz")
-print(f"Prescaler: {p0.prescaler()}")
-print(f"Period: {p0.period()}")
-# Set pulse width to 50%
-p0.pulse_width_percent(100)
+
+
+##################### pin 5
+p5.freq(1000)
+print(f"Frequence: {p5.freq()} Hz")
+print(f"Prescaler: {p5.prescaler()}")
+print(f"Period: {p5.period()}")
+p5.pulse_width_percent(100)
+
+###################### pin 4
+p4.freq(1000)
+print(f"Frequence: {p4.freq()} Hz")
+print(f"Prescaler: {p4.prescaler()}")
+print(f"Period: {p4.period()}")
+p4.pulse_width_percent(100)
