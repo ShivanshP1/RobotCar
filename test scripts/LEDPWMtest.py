dc57edce229212d72679adcbe5db5a84dacd50ee
Mattastrophic0 <mattk2227@gmail.com>
2025-03-17 17:49:17 -0400

    Update LEDPWMtest.py

--- test scripts/LEDPWMtest.py
+++ test scripts/LEDPWMtest.py
@@ -15,16 +15,16 @@ p5 = PWM(5)
 # Set frequency to 1000Hz
 
 ###################### pin 4
-p4.freq(1000)
-print(f"Frequence: {p4.freq()} Hz")
-print(f"Prescaler: {p4.prescaler()}")
-print(f"Period: {p4.period()}")
-p4.pulse_width_percent(100)
+# p4.freq(1000)
+# print(f"Frequence: {p4.freq()} Hz")
+# print(f"Prescaler: {p4.prescaler()}")
+# print(f"Period: {p4.period()}")
+# p4.pulse_width_percent(100)
 ##################### pin 5
-# p5.freq(1000)
-# print(f"Frequence: {p5.freq()} Hz")
-# print(f"Prescaler: {p5.prescaler()}")
-# print(f"Period: {p5.period()}")
-# p5.pulse_width_percent(100)
+p5.freq(1000)
+print(f"Frequence: {p5.freq()} Hz")
+print(f"Prescaler: {p5.prescaler()}")
+print(f"Period: {p5.period()}")
+p5.pulse_width_percent(0)
 
 
