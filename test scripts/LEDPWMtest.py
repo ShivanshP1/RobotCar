dac230a925a64caba6a2e163cb556afc192e3209
Mattastrophic0 <mattk2227@gmail.com>
2025-03-17 17:58:06 -0400

    Update LEDPWMtest.py

--- test scripts/LEDPWMtest.py
+++ test scripts/LEDPWMtest.py
@@ -2,9 +2,9 @@
 from robot_hat import PWM
 
 # Create PWM object with numeric pin numbering and default input pullup enabled
-#p4 = PWM(4)
+p4 = PWM(4)
 p5 = PWM(5)
-print("fuck")
+
 # Create PWM object with named pin numbering
 #p1 = PWM('P1')
 
@@ -15,11 +15,11 @@ print("fuck")
 # Set frequency to 1000Hz
 
 ###################### pin 4
-# p4.freq(1000)
-# print(f"Frequence: {p4.freq()} Hz")
-# print(f"Prescaler: {p4.prescaler()}")
-# print(f"Period: {p4.period()}")
-# p4.pulse_width_percent(100)
+p4.freq(1000)
+print(f"Frequence: {p4.freq()} Hz")
+print(f"Prescaler: {p4.prescaler()}")
+print(f"Period: {p4.period()}")
+p4.pulse_width_percent(100)
 ##################### pin 5
 p5.freq(1000)
 print(f"Frequence: {p5.freq()} Hz")
