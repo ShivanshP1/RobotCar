3d8c57f559c2fa88fdaaf86b49e864d4f57ded07
Mattastrophic0 <mattk2227@gmail.com>
2025-03-25 10:57:30 -0400

    Update drivingPath.py

--- test scripts/PathFindingTest/drivingPath.py
+++ test scripts/PathFindingTest/drivingPath.py
@@ -1,47 +1,56 @@
-import AstarPathFindingOld
+import AstarPathFinding
 import numpy as np
 
 
 direcList = np.vstack(([-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]))
+moveList = []
 def main(src,dest,direcIndex):
 
-    AstarPathFindingOld.main(src, dest)
-    path = AstarPathFindingOld.path
+    AstarPathFinding.main(src, dest)
+    path = AstarPathFinding.path
     #print("path: \n", path)
-    numTurns = 0
-
-    for x in range(len(path[:])-1):
-        #direc = direcList[direcIndex]
+    x =0
+    while x < len(path[:])-1:
         diffPath = path[x + 1] - path[x]
         pathIndex = np.intersect1d(np.where(direcList[:, 0] == diffPath[0]), np.where(direcList[:, 1] == diffPath[1]))
         diffIndex = direcIndex-pathIndex[0]
-        #print("\ndiff path", diffPath )
         print("\npath index",pathIndex[0])
         print("direction index", direcIndex)
         print("diff index", diffIndex)
-        #print("direc:", direc)
-        
+
 
         if direcIndex == pathIndex[0]:
+            moveList.append("f")
             print("move",x," fw")
             numTurns = 0
-        elif  3>= diffIndex >= 1 or -5>= diffIndex >=-7:
+        elif  2>= diffIndex >= 1 or -6>= diffIndex >=-7:
             numTurns = diffIndex % 4
+            if numTurns == 1:
+                moveList.append("l")
+            else:
+                moveList.append("l2")
             print("move",x," fw + left x", numTurns)
             direcIndex = pathIndex[0]
 
-        elif -3>= diffIndex <= -1 or 5>=diffIndex <= 7:
+        elif -2>= diffIndex <= -1 or 6>=diffIndex <= 7:
             numTurns = (diffIndex *-1)%4
+            if numTurns == 1:
+                moveList.append("r")
+            else:
+                moveList.append("r2")
             print("move",x," fw + right x", numTurns )
             direcIndex = pathIndex[0]
         else:
-            x = x-1
             direcIndex = (direcIndex+4)%8
-            print("wamp wamp, do a 180")
-
+            moveList.append("s")
+            print("move",x,"wamp wamp, do a 180")
+            x = x - 1
+        x = x + 1
+    print("\nStop\n")
 
 if __name__ == "__main__":
     src = [2, 62]
     dest = [30, 2]
     initialDirecIndex = 6
     main(src,dest,initialDirecIndex)
+    print(moveList)
